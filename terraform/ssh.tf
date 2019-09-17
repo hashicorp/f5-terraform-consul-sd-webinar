034ede83ac8fd025596c2d0029c9029313652530
Lance Larsen <lance@hashicorp.com>
2019-09-17 08:36:56 -0500

    fmt and clean up old ssh keys

--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -14,4 +14,10 @@ resource "null_resource" "key" {
   provisioner "local-exec" {
     command = "chmod 600 *.pem"
   }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "rm -f *.pem"
+  }
+
 }
