6bf025645310960725e7e8f4e221f32583d3d988
Kevin Corbin <kcorbin@hashicorp.com>
2019-10-16 13:03:42 -0500

    auto download/install as3 rpm (defaults to 3.7.0)

--- as3/variables.tf
+++ as3/variables.tf
@@ -1,3 +1,10 @@
 variable "address" {}
+variable "port" {}
 variable "username" {}
 variable "password" {}
+variable "as3_rpm" {
+  default = "f5-appsvcs-3.7.0-7.noarch.rpm"
+}
+variable "as3_rpm_url" {
+  default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.7.0/f5-appsvcs-3.7.0-7.noarch.rpm"
+}
