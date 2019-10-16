4407c355692ce9498f4b494db9ea05ddae43feb7
Kevin Corbin <kcorbin@hashicorp.com>
2019-10-16 13:01:50 -0500

    add some handy outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "F5_IP" {
 output "F5_Password" {
   value = "${random_string.password.result}"
 }
+
+output "f5_ui" {
+  value = "https://${aws_eip.f5.public_ip}:8443"
+}
+
+output "consul_ui" {
+  value = "http://${aws_instance.consul.public_ip}:8500"
+}
