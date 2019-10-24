0f7cee82d327126a5ad97bffcb81e21abc28a3f2
Eric Chen <eric.chen@f5.com>
2019-10-24 15:35:15 -0400

    use f5-appsvcs-3.15.0-6

--- as3/variables.tf
+++ as3/variables.tf
@@ -3,8 +3,8 @@ variable "port" {}
 variable "username" {}
 variable "password" {}
 variable "as3_rpm" {
-  default = "f5-appsvcs-3.7.0-7.noarch.rpm"
+  default = "f5-appsvcs-3.15.0-6.noarch.rpm"
 }
 variable "as3_rpm_url" {
-  default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.7.0/f5-appsvcs-3.7.0-7.noarch.rpm"
+  default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.15.0/f5-appsvcs-3.15.0-6.noarch.rpm"
 }
