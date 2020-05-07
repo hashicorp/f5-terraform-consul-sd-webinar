a7856e65c92a44d7e352c08de4c9a6fd4028848d
Eric Chen <eric.chen@f5.com>
2020-05-07 13:27:35 -0400

    remove warnings

--- as3/main.tf
+++ as3/main.tf
@@ -1,12 +1,12 @@
 provider "bigip" {
   address  = "https://${var.address}:${var.port}"
-  username = "${var.username}"
-  password = "${var.password}"
+  username = var.username
+  password = var.password
 }
 
 # deploy application using as3
 resource "bigip_as3" "nginx" {
-  as3_json    = "${file("nginx.json")}"
+  as3_json    = file("nginx.json")
   tenant_name = "consul_sd"
 #  depends_on  = [null_resource.install_as3]
 }
