b5de639dce4d7b61e14c0913a2d7e3904fc7e62a
Eric Chen <eric.chen@f5.com>
2020-05-07 11:13:03 -0400

    use tmos runtime init to install AS3

--- as3/main.tf
+++ as3/main.tf
@@ -4,24 +4,9 @@ provider "bigip" {
   password = "${var.password}"
 }
 
-# download rpm
-resource "null_resource" "download_as3" {
-  provisioner "local-exec" {
-    command = "wget ${var.as3_rpm_url}"
-  }
-}
-
-# install rpm to BIG-IP
-resource "null_resource" "install_as3" {
-  provisioner "local-exec" {
-    command = "./install_as3.sh ${var.address}:${var.port} admin:${var.password} ${var.as3_rpm}"
-  }
-  depends_on = [null_resource.download_as3]
-}
-
 # deploy application using as3
 resource "bigip_as3" "nginx" {
   as3_json    = "${file("nginx.json")}"
   tenant_name = "consul_sd"
-  depends_on  = [null_resource.install_as3]
+#  depends_on  = [null_resource.install_as3]
 }
