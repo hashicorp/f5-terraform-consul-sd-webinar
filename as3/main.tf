ed8dd831fa300b97eb861053048455e1d169e45a
Eric Chen <eric.chen@f5.com>
2020-07-02 14:50:44 -0400

    fixes for bigip provider 1.2.1

--- as3/main.tf
+++ as3/main.tf
@@ -4,15 +4,15 @@ provider "bigip" {
   password = var.password
 }
 # pin to 1.1.2
-terraform {
-  required_providers {
-    bigip = "~> 1.1.2"
-  }
-}
+#terraform {
+#  required_providers {
+#    bigip = "~> 1.1.2"
+#  }
+#}
 
 # deploy application using as3
 resource "bigip_as3" "nginx" {
   as3_json    = file("nginx.json")
-  tenant_name = "consul_sd"
+#  tenant_name = "consul_sd"
 #  depends_on  = [null_resource.install_as3]
 }
