178494635d557a6cc7c54119e6b0db2c43df5265
Eric Chen <eric.chen@f5.com>
2020-11-25 03:36:18 -0500

    udpate to 1.9.0

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -4,7 +4,7 @@
 sudo apt-get install unzip
 
 #Download Consul
-CONSUL_VERSION="1.7.2"
+CONSUL_VERSION="1.9.0"
 curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 
 #Install Consul
