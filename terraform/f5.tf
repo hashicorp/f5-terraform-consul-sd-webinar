27406e71ced67cfc0e66e1410288faf32a39ddbb
Eric Chen <eric.chen@f5.com>
2020-12-19 21:28:15 -0500

    reference module by git repo;use custom user_data

--- terraform/f5.tf
+++ terraform/f5.tf
@@ -1,6 +1,27 @@
+resource "random_string" "password" {
+  length  = 10
+  special = false
+}
+
+# encrypt password sha512
+resource "null_resource" "admin-shadow" {
+  provisioner "local-exec" {
+    command = "./admin-shadow.sh ${random_string.password.result}"
+  }
+}
+
+data "template_file" "f5_init" {
+  template = "${file("../scripts/f5.tpl")}"
+
+  vars = {
+    encrypted_password = chomp(file("admin.shadow"))
+  }
+  depends_on = [null_resource.admin-shadow]  
+}
+
 module bigip {
   count                  = 1
-  source                 = "../../terraform-aws-bigip-module"
+  source                 = "git::https://github.com/f5devcentral/terraform-aws-bigip-module?ref=0.9"
   prefix                 = "bigip-aws-1nic"
   ec2_instance_type      = "m5.large"
   ec2_key_name           = aws_key_pair.demo.key_name
@@ -8,5 +29,5 @@ module bigip {
   f5_username            = var.f5_username
   mgmt_subnet_ids        = [{ "subnet_id" = module.vpc.public_subnets[0], "public_ip" = true, "private_ip_primary" = "10.0.0.200" }]
   mgmt_securitygroup_ids = [aws_security_group.f5.id]
-  AS3_URL                = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.24.0/f5-appsvcs-3.24.0-5.noarch.rpm"
+  custom_user_data       = data.template_file.f5_init.rendered
 }
