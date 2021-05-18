5e8e99ac43578edb2a5cd442407549b9df6bb664
Eric Chen <eric.chen@f5.com>
2021-05-18 06:48:28 -0400

    use FAST for Event-Driven Service Discovery/NIA

---
+++ fast/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    bigip = {
+      source  = "F5Networks/bigip"
+      version = "1.9.0"
+    }
+  }
+}
+provider "bigip" {
+  address  = "https://${var.address}:${var.port}"
+  username = var.username
+  password = var.password
+}
+
+# generate zip file
+
+data "archive_file" "template_zip" {
+  type        = "zip"
+  source_file = "ConsulWebinar.yaml"
+  output_path = "ConsulWebinar.zip"
+}
+
+# deploy fast template
+
+resource "bigip_fast_template" "consul-webinar" {
+  name = "ConsulWebinar"
+  source = "ConsulWebinar.zip"
+  depends_on = [data.archive_file.template_zip]
+}
+
+resource "bigip_fast_application" "nginx-webserver" {
+  template        = "ConsulWebinar/ConsulWebinar"
+  fast_json   = <<EOF
+{
+      "tenant": "Consul_SD",
+      "app": "Nginx",
+      "virtualAddress": "10.0.0.200",
+      "virtualPort": 8080
+}
+EOF
+  depends_on = [bigip_fast_template.consul-webinar]
+}
---
+++ fast/variables.tf
@@ -0,0 +1,7 @@
+variable "address" {}
+variable "port" {}
+variable "username" {}
+variable "password" {}
+variable "declaration" {
+  default = "nginx.json"
+}
